05c88027f956574960fe90454e84d4cffda74c74
dabooom <dabooom93@gmail.com>
2021-01-19 11:52:47 +0900

    0119_js_var in array

--- www/NCS/js/boksoon.js
+++ www/NCS/js/boksoon.js
@@ -1,18 +1,35 @@
+//
+// $(function(){
+//
+//   var gnbTag="";
+//   var gnbData=[
+//                 ["list1","","ABOUT"],
+//                 ["list2","","OFFLINE"],
+//                 ["list3","","PRODUCT"],
+//                 ["list4","","EVENT"],
+//                ];
+//
+//    for(var x=0; x<gnbData.length; x++){
+//    gnbTag+="<li class="+gnbData[x][0]+"><a href="+gnbData[x][1]+">"+gnbData[x][2]+"</a></li>";
+//    }
+//    $("#gnb").html(gnbTag);
+//
+// });
 
 $(function(){
 
-  var gnbTag="";
-  var gnbData=[
-                ["list1","","ABOUT"],
-                ["list2","","OFFLINE"],
-                ["list3","","PRODUCT"],
-                ["list4","","EVENT"],
-               ];
+    var gnbTag ="";
+    var gnbJson ={
+                 gnblistClass:["list1","list2","list3","list4"],
+                 gnblistLink:["#","#","#","#"],
+                 gnblistText:["ABOUT","OFFLINE","PRODUCT","EVENT"],
+                }; ////gnbData
+    ///////////////////////실행문///////////////////////
+    for(var i in gnbJson.gnblistClass){
+      gnbTag += "<li class='"+gnbJson.gnblistClass[i]+"' >";
+      gnbTag += "<a href='"+gnbJson.gnblistLink[i]+"' >";
+      gnbTag += gnbJson.gnblistText[i]+"</a></li>"
+    }
+    $("#gnb").html(gnbTag);
 
-   for(var x=0; x<gnbData.length; x++){
-   gnbTag+="<li class="+gnbData[x][0]+"><a href="+gnbData[x][1]+">"+gnbData[x][2]+"</a></li>";
-   }
-   $("#gnb").html(gnbTag);
-
-
-});
+    });
